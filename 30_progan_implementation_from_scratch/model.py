3cbe2a9d508536bdc0b4ca6e813b2cba4d53fb46
Ekomobong Archibong <59149177+archie-bryann@users.noreply.github.com>
2025-12-05 09:43:56 +0100

    progan implementation from scratch

--- 30_progan_implementation_from_scratch/model.py
+++ 30_progan_implementation_from_scratch/model.py
@@ -84,7 +84,7 @@ class Generator(nn.Module):
         return self.fade_in(alpha, final_upscaled, final_out)
 
 class Discriminator(nn.Module):
-    def __init__(self, z_dim, in_channels, img_channels=3):
+    def __init__(self, in_channels, img_channels=3):
         super().__init__()
         self.prog_blocks, self.rgb_layers = nn.ModuleList(), nn.ModuleList()
         self.leaky = nn.LeakyReLU(0.2)
@@ -97,22 +97,23 @@ class Discriminator(nn.Module):
 
         # this is for 4x4 img resolution
         self.initial_rgb = WSConv2d(img_channels, in_channels, kernel_size=1, stride=1, padding=0)
+        self.rgb_layers.append(self.initial_rgb)
         self.avg_pool = nn.AvgPool2d(kernel_size=2, stride=2)
 
         # block for 4x4 resolution
         self.final_block = nn.Sequential(
             WSConv2d(in_channels+1, in_channels, kernel_size=3, stride=1, padding=1),
             nn.LeakyReLU(0.2),
-            WSConv2d(in_channels, in_channels, kernel_size=4, padding=0, stride=1),
+            WSConv2d(in_channels, in_channels, kernel_size=4, stride=1,  padding=0),
             nn.LeakyReLU(0.2),
-            WSConv2d(in_channels, 1, kernel_size=1, padding=0, stride=1),
+            WSConv2d(in_channels, 1, kernel_size=1,  stride=1, padding=0),
         )
     
     def fade_in(self, alpha, downscaled, out):
         return alpha * out + (1 - alpha) * downscaled
 
     def minibatch_std(self, x):
-        batch_statistics = torch.std(x, dim=0).mean().repeat(x.shape[0], 1, x.shape[2], x.shape[3])
+        batch_statistics = torch.std(x, dim=0, unbiased=False).mean().repeat(x.shape[0], 1, x.shape[2], x.shape[3])
         return torch.cat([x, batch_statistics], dim=1)
 
     def forward(self, x, alpha, steps): # steps=0 (4x4), steps=1 (8x8), etc.
@@ -133,3 +134,18 @@ class Discriminator(nn.Module):
 
         out = self.minibatch_std(out)
         return self.final_block(out).view(out.shape[0], -1)
+
+if __name__ == "__main__":
+    Z_DIM = 512
+    IN_CHANNELS = 256
+    gen = Generator(Z_DIM, IN_CHANNELS, img_channels=3)
+    critic = Discriminator(IN_CHANNELS, img_channels=3)
+
+    for img_size in [4, 8, 16, 32, 64, 128, 256, 512, 1024]:
+        num_steps = int(log2(img_size / 4))
+        x = torch.randn((1, Z_DIM, 1, 1))
+        z = gen(x, 0.5, steps=num_steps)
+        assert z.shape == (1, 3, img_size, img_size)
+        out = critic(z, alpha=0.5, steps=num_steps)
+        assert out.shape == (1, 1)
+        print(f"Success! At img size: {img_size}")
