4ed9df9c0f801faf8251f657ecece43f4418b0a6
Ekomobong Archibong <59149177+archie-bryann@users.noreply.github.com>
2025-12-10 15:36:59 +0100

    Non Max Suppression Explained and PyTorch Implementation

---
+++ 43_non_max_suppression/iou.py
@@ -0,0 +1,57 @@
+import torch
+
+def intersection_over_union(boxes_preds, boxes_labels, box_format=""):
+    """
+    Calculates intersection over union
+    
+    Parameters:
+        boxes_preds (tensor): Predictions of Bounding Boxes (BATCH_SIZE, 4)
+        boxes_labels (tensor): Correct labels of Bounding Boxes (BATCH_SIZE, 4)
+        box_format (str): midpoint/corners, if boxes (x,y,w,h) or (x1,y1,x2,y2)
+    
+    Returns:
+        tensor: Intersection over union for all examples
+    """
+    
+    # boxes_preds shape is (N, 4) where N is the number of bboxes
+    # boxes_labels shape is (N, 4)
+    # Extract bounding box coordinates for box1
+
+    if box_format == "midpoint":
+        # Box1 (predictions) - center coordinates and dimensions
+        box1_x1 = boxes_preds[..., 0:1] - boxes_preds[..., 2:3] / 2
+        box1_y1 = boxes_preds[..., 1:2] - boxes_preds[..., 3:4] / 2
+        box1_x2 = boxes_preds[..., 0:1] + boxes_preds[..., 2:3] / 2
+        box1_y2 = boxes_preds[..., 1:2] + boxes_preds[..., 3:4] / 2
+        
+        # Box2 (labels) - center coordinates and dimensions
+        box2_x1 = boxes_labels[..., 0:1] - boxes_labels[..., 2:3] / 2
+        box2_y1 = boxes_labels[..., 1:2] - boxes_labels[..., 3:4] / 2
+        box2_x2 = boxes_labels[..., 0:1] + boxes_labels[..., 2:3] / 2
+        box2_y2 = boxes_labels[..., 1:2] + boxes_labels[..., 3:4] / 2
+        
+
+    if box_format == "corners":
+        box1_x1 = boxes_preds[..., 0:1]
+        box1_y1 = boxes_preds[..., 1:2]
+        box1_x2 = boxes_preds[..., 2:3]
+        box1_y2 = boxes_preds[..., 3:4 # (N, 1)
+        
+        # Extract bounding box coordinates for box2
+        box2_x1 = boxes_labels[..., 0:1]
+        box2_y1 = boxes_labels[..., 1:2]
+        box2_x2 = boxes_labels[..., 2:3]
+        box2_y2 = boxes_labels[..., 3:4]
+
+    x1 = torch.max(box1_x1, box2_x1)
+    y1 = torch.max(box1_y1, box2_y1)
+    x2 = torch.min(box1_x2, box2_x2)
+    y2 = torch.min(box1_y2, box2_y2)
+
+    # .clamp(0) is for th ecase when they do not intersect.
+    intersection = (x2 - x1).clamp(0) * (y2 - y1).clamp(0
+
+    box1_area = abs((box1_x2 - box1_x1) * (box1_y1 - box1_y2))
+    box2_are = abs((box2_x2 - box2_x1) * (box2_y1 - box2_y2))
+
+    return intersection / (box1_area + box2_area - intersections + 1e-6)
